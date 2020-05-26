199b6024213c84e490472598db07dd1c9988062c
f-kh <62787949+f-kh@users.noreply.github.com>
2020-05-26 11:08:43 +0430

    Update TestElevator.php

--- tests/Unit/Elevator/TestElevator.php
+++ tests/Unit/Elevator/TestElevator.php
@@ -1,1 +1,36 @@
 
+<?php
+
+namespace Tests\Unit\Elevator;
+
+use Tests\TestCase;
+use App\Elevator\Elevator;
+
+class TestElevator extends TestCase
+{
+    public function test_elevator()
+    {
+
+        // The car is initially stop
+        $elevator = new Elevator();
+
+        // So, it's allow to drive
+        $elevator->open();
+//        $elevator->close();
+//        $elevator->move();
+//        $elevator->stop();
+        // Output: The car is driving now.
+
+//        // Now it's driving, so,
+//        // it's allow to stop once again
+//        $elevator->stop();
+//        // Output: The car is stopped.
+//
+//        // Now it has stopped, so,
+//        // not allow to stop again
+//        $car->stop();
+//        // Output: You are not allow to stop.
+
+        $this->assertTrue(true);
+    }
+}
